db9e222cc551cbce967c37e9b24c0a58e9da6ce9
brynkinz2 <sweettewbme@gmail.com>
2025-09-18 11:42:36 -0600

    Add equals, hash, and toString methods.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Represents a single chess piece
@@ -10,7 +12,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private ChessGame.TeamColor color;
+    private PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.color = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -29,14 +36,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return color;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -47,6 +54,34 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        List<ChessMove> moves = new ArrayList<>();
+        if (getPieceType() == PieceType.KING) {}
+        else if (getPieceType() == PieceType.QUEEN) {}
+        else if (getPieceType() == PieceType.BISHOP) {
+            int currRow = myPosition.getRow();
+            int currCol = myPosition.getColumn();
+            int directions[][] = {{-1, 1}, {1,1}, {1,-1}, {-1,-1}};
+
+            for (int[] direction : directions) {
+                int checkRow = currRow + direction[0];
+                int checkCol = currCol + direction[1];
+
+                while (checkRow >= 0 && checkCol >= 0 && checkRow < 8 && checkCol < 8) {
+                    ChessPiece checkPiece = board.getPiece(new ChessPosition(checkRow, checkCol));
+                    if (checkPiece == null) {
+                        moves.add(new ChessMove(myPosition, new ChessPosition(checkRow, checkCol), null));
+                    }
+                    else if (checkPiece.getTeamColor() != color) {
+                        moves.add(new ChessMove(myPosition, new ChessPosition(checkRow, checkCol), null));
+                        break;
+                    }
+                }
+            }
+
+        }
+        else if (getPieceType() == PieceType.KNIGHT) {}
+        else if (getPieceType() == PieceType.ROOK) {}
+        else if (getPieceType() == PieceType.PAWN) {}
+        return moves;
     }
 }
